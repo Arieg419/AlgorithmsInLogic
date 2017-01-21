cd1fbc481b1e3522ab14fe2416b405a0fa615f06
Omer Goldberg <arieg419@gmail.com>
2017-01-21 21:08:36 +0200

    building final project, preppeed fake data, now building form

---
+++ HW2/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);  
+
+ReactDOM.render(
+	)
+
+
